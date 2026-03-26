ea5a190081546d6a15cc359a26254008ff655363
Sam Mosios <sam.mosios@gmail.com>
2026-03-26 15:26:45 +0100

    Adds ids, trustmap entries

--- assignment-1/dating/out/dating-client.js
+++ assignment-1/dating/out/dating-client.js
@@ -4,12 +4,315 @@ function Top (rt) {
   this.addLib = function (lib, decl) { if (!this.libSet.has (lib +'.'+decl)) { this.libSet.add (lib +'.'+decl); this.libs.push ({lib:lib, decl:decl})} }
   this.loadlibs = function (cb) { rt.linkLibs (this.libs, this, cb) }
   this.serializedatoms = "AQAAAAAAAAAA"
-  this.main = function ($env,$$authorityarg) {
-    const gensym1 = rt.mkValPos (0,':4:1');;
-    rt.ret (gensym1);
+  this.print2 = ($env) => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 1]  = _T.checkDataBoundsEntry($env.__dataLevel)
+    _T.boundSlot =  _SP + 1
+    const _$reg0_val = _T.r0_val;
+    let _$reg0_lev = _T.pc;
+    let _$reg0_tlev = _T.pc;
+    let _pc_init = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      _$reg0_lev = _T.r0_lev;
+      _$reg0_tlev = _T.r0_tlev;
+      _pc_init = _T.pc;
+    }
+    const print_arg15 = rt.constructLVal (_$reg0_val,_$reg0_lev,_$reg0_tlev);
+    _STACK[ _SP + 0] =  print_arg15
+    const lval1 = rt. getStdout;
+    const _raw_2 = lval1.val;
+    const _val_13 = $env.gensym46.val;
+    const _vlev_14 = $env.gensym46.lev;
+    const _tlev_15 = $env.gensym46.tlev;
+    rt.rawAssertIsFunction (_raw_2);
+    let _bl_12 = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      const _bl_10 = _T.bl;
+      _bl_12 = rt.join (_bl_10,_pc_init);;
+    }
+    _SP_OLD = _SP; 
+    _SP = _SP +  7 ;
+    _STACK[_SP - 5] = _SP_OLD;
+    _STACK[_SP - 4] = _T.pc;
+    _STACK[_SP - 3] = this.$$$print2$$$kont0
+    _STACK[_SP - 2] = _T.mailbox.mclear;
+    _STACK[_SP - 1] = false;
+    _T._sp = _SP;
+    if (! _STACK[ _SP + -6] ) {
+      _T.pc = _pc_init;
+      _T.bl = rt.wrap_block_rhs (_bl_12);
+    }
+    _T.r0_val = _val_13;
+    _T.r0_lev = _vlev_14;
+    _T.r0_tlev = _tlev_15;
+    return _raw_2
   }
-  this.main.deps = [];
+  this.print2.deps = [];
+  this.print2.libdeps = [];
+  this.print2.serialized = "AAAAAAAAAAAGcHJpbnQyAAAAAAAAAAtwcmludF9hcmcxNQAAAAAAAAAAAAAAAAAAAAAGAAAAAAAAAAskZGVjbHRlbXAkOQAAAAAAAAABAAAAAAAAAAAHZ2Vuc3ltNQkAAAAAAAAACWdldFN0ZG91dAAAAAAAAAAAAAdnZW5zeW01AQAAAAAAAAAIZ2Vuc3ltNDYAAAAAAAAAAgAAAAAAAAAAB2dlbnN5bTMJAAAAAAAAAAhmcHJpbnRsbgAAAAAAAAAAB2dlbnN5bTQCAAAAAAAAAAIAAAAAAAAAAAskZGVjbHRlbXAkOQAAAAAAAAAAC3ByaW50X2FyZzE1AAAAAAAAAAAAB2dlbnN5bTMAAAAAAAAAAAdnZW5zeW00";
+  this.print2.framesize = 1;
+  this.printWithLabels3 = ($env) => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 1]  = _T.checkDataBoundsEntry($env.__dataLevel)
+    _T.boundSlot =  _SP + 1
+    const _$reg0_val = _T.r0_val;
+    let _$reg0_lev = _T.pc;
+    let _$reg0_tlev = _T.pc;
+    let _pc_init = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      _$reg0_lev = _T.r0_lev;
+      _$reg0_tlev = _T.r0_tlev;
+      _pc_init = _T.pc;
+    }
+    const printWithLabels_arg111 = rt.constructLVal (_$reg0_val,_$reg0_lev,_$reg0_tlev);
+    _STACK[ _SP + 0] =  printWithLabels_arg111
+    const lval1 = rt. getStdout;
+    const _raw_2 = lval1.val;
+    const _val_13 = $env.gensym46.val;
+    const _vlev_14 = $env.gensym46.lev;
+    const _tlev_15 = $env.gensym46.tlev;
+    rt.rawAssertIsFunction (_raw_2);
+    let _bl_12 = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      const _bl_10 = _T.bl;
+      _bl_12 = rt.join (_bl_10,_pc_init);;
+    }
+    _SP_OLD = _SP; 
+    _SP = _SP +  7 ;
+    _STACK[_SP - 5] = _SP_OLD;
+    _STACK[_SP - 4] = _T.pc;
+    _STACK[_SP - 3] = this.$$$printWithLabels3$$$kont1
+    _STACK[_SP - 2] = _T.mailbox.mclear;
+    _STACK[_SP - 1] = false;
+    _T._sp = _SP;
+    if (! _STACK[ _SP + -6] ) {
+      _T.pc = _pc_init;
+      _T.bl = rt.wrap_block_rhs (_bl_12);
+    }
+    _T.r0_val = _val_13;
+    _T.r0_lev = _vlev_14;
+    _T.r0_tlev = _tlev_15;
+    return _raw_2
+  }
+  this.printWithLabels3.deps = [];
+  this.printWithLabels3.libdeps = [];
+  this.printWithLabels3.serialized = "AAAAAAAAAAAQcHJpbnRXaXRoTGFiZWxzMwAAAAAAAAAWcHJpbnRXaXRoTGFiZWxzX2FyZzExMQAAAAAAAAAAAAAAAAAAAAAGAAAAAAAAAAwkZGVjbHRlbXAkMTUAAAAAAAAAAQAAAAAAAAAACGdlbnN5bTE5CQAAAAAAAAAJZ2V0U3Rkb3V0AAAAAAAAAAAACGdlbnN5bTE5AQAAAAAAAAAIZ2Vuc3ltNDYAAAAAAAAAAgAAAAAAAAAACGdlbnN5bTE3CQAAAAAAAAASZnByaW50bG5XaXRoTGFiZWxzAAAAAAAAAAAIZ2Vuc3ltMTgCAAAAAAAAAAIAAAAAAAAAAAwkZGVjbHRlbXAkMTUAAAAAAAAAABZwcmludFdpdGhMYWJlbHNfYXJnMTExAAAAAAAAAAAACGdlbnN5bTE3AAAAAAAAAAAIZ2Vuc3ltMTg=";
+  this.printWithLabels3.framesize = 1;
+  this.printString4 = ($env) => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 4]  = _T.checkDataBoundsEntry($env.__dataLevel)
+    _T.boundSlot =  _SP + 4
+    const gensym34$$$const = "\n"
+    const _$reg0_val = _T.r0_val;
+    _STACK[ _SP + 2] =  _$reg0_val
+    const lval1 = rt. getStdout;
+    const _raw_2 = lval1.val;
+    const _val_13 = $env.gensym46.val;
+    const _vlev_14 = $env.gensym46.lev;
+    const _tlev_15 = $env.gensym46.tlev;
+    rt.rawAssertIsFunction (_raw_2);
+    let _$reg0_lev = _T.pc;
+    let _$reg0_tlev = _T.pc;
+    let _pc_init = _T.pc;
+    let _bl_12 = _T.pc;
+    if (! _STACK[ _SP + 4] ) {
+      _$reg0_lev = _T.r0_lev;
+      _$reg0_tlev = _T.r0_tlev;
+      _pc_init = _T.pc;
+      const _bl_10 = _T.bl;
+      _bl_12 = rt.join (_bl_10,_pc_init);;
+    }
+    _STACK[ _SP + 0] =  _$reg0_lev
+    _STACK[ _SP + 1] =  _$reg0_tlev
+    _STACK[ _SP + 3] =  _pc_init
+    _SP_OLD = _SP; 
+    _SP = _SP +  10 ;
+    _STACK[_SP - 5] = _SP_OLD;
+    _STACK[_SP - 4] = _T.pc;
+    _STACK[_SP - 3] = this.$$$printString4$$$kont2
+    _STACK[_SP - 2] = _T.mailbox.mclear;
+    _STACK[_SP - 1] = false;
+    _T._sp = _SP;
+    if (! _STACK[ _SP + -6] ) {
+      _T.pc = _pc_init;
+      _T.bl = rt.wrap_block_rhs (_bl_12);
+    }
+    _T.r0_val = _val_13;
+    _T.r0_lev = _vlev_14;
+    _T.r0_tlev = _tlev_15;
+    return _raw_2
+  }
+  this.printString4.deps = [];
+  this.printString4.libdeps = [];
+  this.printString4.serialized = "AAAAAAAAAAAMcHJpbnRTdHJpbmc0AAAAAAAAABJwcmludFN0cmluZ19hcmcxMTcAAAAAAAAAAQAAAAAAAAAIZ2Vuc3ltMzQBAAAAAAAAAAJcbgAAAAAAAAAABgAAAAAAAAAMJGRlY2x0ZW1wJDIxAAAAAAAAAAEAAAAAAAAAAAhnZW5zeW0zNQkAAAAAAAAACWdldFN0ZG91dAAAAAAAAAAAAAhnZW5zeW0zNQEAAAAAAAAACGdlbnN5bTQ2AAAAAAAAAAMAAAAAAAAAAAhnZW5zeW0zMQkAAAAAAAAABmZ3cml0ZQAAAAAAAAAACGdlbnN5bTMyABAAAAAAAAAAABJwcmludFN0cmluZ19hcmcxMTcAAAAAAAAAAAhnZW5zeW0zNAAAAAAAAAAACGdlbnN5bTMzAgAAAAAAAAACAAAAAAAAAAAMJGRlY2x0ZW1wJDIxAAAAAAAAAAAIZ2Vuc3ltMzIAAAAAAAAAAAAIZ2Vuc3ltMzEAAAAAAAAAAAhnZW5zeW0zMw==";
+  this.printString4.framesize = 4;
+  this.main = ($env) => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 0]  = _T.checkDataBoundsEntry($env.__dataLevel)
+    _T.boundSlot =  _SP + 0
+    const gensym45$$$const = 0
+    const _$reg0_val = _T.r0_val;
+    let _pc_init = _T.pc;
+    let _raw_4 = _T.pc;
+    let _raw_5 = _T.pc;
+    if (! _STACK[ _SP + 0] ) {
+      const _$reg0_lev = _T.r0_lev;
+      const _$reg0_tlev = _T.r0_tlev;
+      _pc_init = _T.pc;
+      _raw_4 = rt.join (_pc_init,_$reg0_lev);;
+      _raw_5 = rt.join (_pc_init,_$reg0_tlev);;
+    }
+    const gensym46 = rt.constructLVal (_$reg0_val,_raw_4,_raw_5);
+    const $$$env3 = new rt.Env();
+    $$$env3.gensym46 = gensym46;
+    $$$env3.__dataLevel =  rt.join (gensym46.dataLevel);
+    const print2 = rt.mkVal(rt.RawClosure($$$env3, this, this.print2))
+    $$$env3.print2 = print2;
+    $$$env3.print2.selfpointer = true;
+    const printWithLabels3 = rt.mkVal(rt.RawClosure($$$env3, this, this.printWithLabels3))
+    $$$env3.printWithLabels3 = printWithLabels3;
+    $$$env3.printWithLabels3.selfpointer = true;
+    const printString4 = rt.mkVal(rt.RawClosure($$$env3, this, this.printString4))
+    $$$env3.printString4 = printString4;
+    $$$env3.printString4.selfpointer = true;
+    _T.r0_val = gensym45$$$const;
+    _T.r0_lev = _pc_init;
+    _T.r0_tlev = _pc_init;
+    return _T.returnImmediate ();
+  }
+  this.main.deps = ['print2', 'printWithLabels3', 'printString4'];
   this.main.libdeps = [];
-  this.main.serialized = "AAAAAAAAAAAEbWFpbgAAAAAAAAAOJCRhdXRob3JpdHlhcmcAAAAAAAAAAQAAAAAAAAAAB2dlbnN5bTEFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAQEAAAAAAAAAAAdnZW5zeW0x";
+  this.main.serialized = "AAAAAAAAAAAEbWFpbgAAAAAAAAAOJCRhdXRob3JpdHlhcmcAAAAAAAAAAQAAAAAAAAAIZ2Vuc3ltNDUAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAABAAAAAAAAAAIAAAAAAAAAAAhnZW5zeW00NgkAAAAAAAAADiQkYXV0aG9yaXR5YXJnAQAAAAAAAAABAAAAAAAAAAhnZW5zeW00NgAAAAAAAAAACGdlbnN5bTQ2AAAAAAAAAAMAAAAAAAAABnByaW50MgAAAAAAAAAGcHJpbnQyAAAAAAAAABBwcmludFdpdGhMYWJlbHMzAAAAAAAAABBwcmludFdpdGhMYWJlbHMzAAAAAAAAAAxwcmludFN0cmluZzQAAAAAAAAADHByaW50U3RyaW5nNAEAAAAAAAAAAAhnZW5zeW00NQ==";
+  this.main.framesize = 0;
+  this.$$$print2$$$kont0 = () => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 1] = _T.checkDataBounds( _STACK[ _SP + 1] )
+    _T.boundSlot = _SP + 1
+    const print_arg15 = _STACK[ _SP + 0]
+    const _r0_val_37 = _T.r0_val;
+    let _r0_lev_38 = _T.pc;
+    let _r0_tlev_39 = _T.pc;
+    let _pc_16 = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      _r0_lev_38 = _T.r0_lev;
+      _r0_tlev_39 = _T.r0_tlev;
+      _pc_16 = _T.pc;
+    }
+    const $decltemp$9 = rt.constructLVal (_r0_val_37,_r0_lev_38,_r0_tlev_39);
+    const lval17 = rt. fprintln;
+    const _raw_18 = lval17.val;
+    const _raw_23 = rt.mkTuple([$decltemp$9, print_arg15]);
+    rt.rawAssertIsFunction (_raw_18);
+    if (! _STACK[ _SP + 1] ) {
+      const _bl_31 = _T.bl;
+      const _bl_33 = rt.join (_bl_31,_pc_16);;
+      _T.pc = _pc_16;
+      _T.bl = rt.wrap_block_rhs (_bl_33);
+    }
+    _T.r0_val = _raw_23;
+    _T.r0_lev = _pc_16;
+    _T.r0_tlev = _pc_16;
+    return _raw_18
+  }
+  this.$$$print2$$$kont0.debugname = "$$$print2$$$kont0"
+  this.$$$printWithLabels3$$$kont1 = () => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 1] = _T.checkDataBounds( _STACK[ _SP + 1] )
+    _T.boundSlot = _SP + 1
+    const printWithLabels_arg111 = _STACK[ _SP + 0]
+    const _r0_val_37 = _T.r0_val;
+    let _r0_lev_38 = _T.pc;
+    let _r0_tlev_39 = _T.pc;
+    let _pc_16 = _T.pc;
+    if (! _STACK[ _SP + 1] ) {
+      _r0_lev_38 = _T.r0_lev;
+      _r0_tlev_39 = _T.r0_tlev;
+      _pc_16 = _T.pc;
+    }
+    const $decltemp$15 = rt.constructLVal (_r0_val_37,_r0_lev_38,_r0_tlev_39);
+    const lval17 = rt. fprintlnWithLabels;
+    const _raw_18 = lval17.val;
+    const _raw_23 = rt.mkTuple([$decltemp$15, printWithLabels_arg111]);
+    rt.rawAssertIsFunction (_raw_18);
+    if (! _STACK[ _SP + 1] ) {
+      const _bl_31 = _T.bl;
+      const _bl_33 = rt.join (_bl_31,_pc_16);;
+      _T.pc = _pc_16;
+      _T.bl = rt.wrap_block_rhs (_bl_33);
+    }
+    _T.r0_val = _raw_23;
+    _T.r0_lev = _pc_16;
+    _T.r0_tlev = _pc_16;
+    return _raw_18
+  }
+  this.$$$printWithLabels3$$$kont1.debugname = "$$$printWithLabels3$$$kont1"
+  this.$$$printString4$$$kont2 = () => {
+    let _T = rt.runtime.$t
+    let _STACK = _T.callStack
+    let _SP = _T._sp
+    let _SP_OLD
+    _STACK[ _SP + 4] = _T.checkDataBounds( _STACK[ _SP + 4] )
+    _T.boundSlot = _SP + 4
+    const gensym34$$$const = "\n"
+    const _$reg0_lev = _STACK[ _SP + 0]
+    const _$reg0_tlev = _STACK[ _SP + 1]
+    const _$reg0_val = _STACK[ _SP + 2]
+    const _pc_init = _STACK[ _SP + 3]
+    const _r0_val_55 = _T.r0_val;
+    let _r0_lev_56 = _T.pc;
+    let _r0_tlev_57 = _T.pc;
+    let _pc_16 = _T.pc;
+    if (! _STACK[ _SP + 4] ) {
+      _r0_lev_56 = _T.r0_lev;
+      _r0_tlev_57 = _T.r0_tlev;
+      _pc_16 = _T.pc;
+    }
+    const $decltemp$21 = rt.constructLVal (_r0_val_55,_r0_lev_56,_r0_tlev_57);
+    const lval17 = rt. fwrite;
+    const _raw_18 = lval17.val;
+    rt.rawAssertIsString (_$reg0_val);
+    const _raw_33 = _$reg0_val + gensym34$$$const;
+    let _bl_31 = _T.pc;
+    let _raw_38 = _T.pc;
+    if (! _STACK[ _SP + 4] ) {
+      const _bl_28 = _T.bl;
+      const _bl_29 = rt.join (_bl_28,_$reg0_tlev);;
+      _bl_31 = rt.join (_bl_29,_pc_init);;
+      const _raw_34 = rt.join (_$reg0_lev,_pc_init);;
+      const _raw_36 = rt.join (_raw_34,_pc_16);;
+      _raw_38 = rt.join (_pc_16,_raw_36);;
+    }
+    const gensym32 = rt.constructLVal (_raw_33,_raw_38,_pc_16);
+    const _raw_41 = rt.mkTuple([$decltemp$21, gensym32]);
+    rt.rawAssertIsFunction (_raw_18);
+    if (! _STACK[ _SP + 4] ) {
+      const _bl_51 = rt.join (_bl_31,_pc_16);;
+      _T.pc = _pc_16;
+      _T.bl = rt.wrap_block_rhs (_bl_51);
+    }
+    _T.r0_val = _raw_41;
+    _T.r0_lev = _pc_16;
+    _T.r0_tlev = _pc_16;
+    return _raw_18
+  }
+  this.$$$printString4$$$kont2.debugname = "$$$printString4$$$kont2"
 }
 module.exports = Top 
